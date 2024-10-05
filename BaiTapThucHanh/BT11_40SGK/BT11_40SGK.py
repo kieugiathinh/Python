962f93ec87c7887b80e2974bd2c5f28380aaf879
Gia Thịnh <127721400+kieugiathinh@users.noreply.github.com>
2024-10-05 23:33:43 +0700

    LiuLiu

---
+++ BaiTapThucHanh/BT11_40SGK/BT11_40SGK.py
@@ -0,0 +1,21 @@
+﻿#Tìm UCLN của 2 số
+
+#Hàm tìm ước số chung lớn nhất
+def UCLN(a, b):
+    ucln = 1
+    for i in range (1, min(a, b) + 1):
+        if a %i == 0 and b %i == 0:
+            ucln = i
+    return ucln
+
+#Hàm Main()
+def Main():
+    a = int(input("Nhập vào số nguyên dương a = "))
+    b = int(input("Nhập vào số nguyên dương b = "))
+      
+    ucln = UCLN(a, b)
+    print(f"Ước chung lớn nhất của {a} và {b} là: {ucln} ")
+    
+    print()
+
+Main()
