f0625d224ec944686c00cbb993ad0b77fd52cd37
Gia Thịnh <127721400+kieugiathinh@users.noreply.github.com>
2024-10-05 23:33:14 +0700

    LiuLiu

---
+++ BaiTapThucHanh/BT10_40SGK/BT10_40SGK.py
@@ -0,0 +1,15 @@
+﻿#In ra tất cae các ước của N
+
+def TatCaUoc(n):
+    for i in range (1, n + 1):
+        if n % i == 0:
+            print(i, end=" ")
+
+def Main():
+    n = int(input("Nhập vào số nguyên dương n = "))
+    print(f"Tất cả ước của {n} là: ", end="")
+    TatCaUoc(n)
+
+    print()
+
+Main()
